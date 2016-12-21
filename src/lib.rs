9a6ec17b1d99bc7d5538f6c967459128ad38702d
sinkuu <sinkuupump@gmail.com>
2016-12-21 10:00:39 +0900

    Move geodetic system parameter to GeodeticCoord

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 extern crate euclid;
+#[macro_use] extern crate approx;
 
 use euclid::{Degrees, Radians};
+use approx::ApproxEq;
 
 pub mod ellipsoid;
 use ellipsoid::*;
@@ -32,14 +34,14 @@ impl RadiansExt<f64> for Radians<f64> {
 }
 
 /// A position on the earth represented by latitude [rad], longitude [rad], and geoid height [m].
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct GeodeticCoord {
+pub struct GeodeticCoord<E> {
     pub lat: Radians<f64>,
     pub lon: Radians<f64>,
     pub hgt: f64,
+    _ellipsoid: PhantomData<E>,
 }
 
-impl GeodeticCoord {
+impl<E> GeodeticCoord<E> {
     /// Construct a geodetic coord with latitude/longitude in radians.
     #[inline]
     pub fn new(lat: Radians<f64>, lon: Radians<f64>, hgt: f64) -> Self {
@@ -47,6 +49,7 @@ impl GeodeticCoord {
             lat: lat,
             lon: lon,
             hgt: hgt,
+            _ellipsoid: PhantomData,
         }
     }
 
@@ -57,64 +60,132 @@ impl GeodeticCoord {
             lat: lat.to_radians(),
             lon: lon.to_radians(),
             hgt: hgt,
+            _ellipsoid: PhantomData,
         }
     }
 }
 
-/// A position on the earth represented by x [m], y [m], and z [m] in geocentric (ECEF) coordinate.
-#[derive(Clone, Copy)]
-pub struct GeocentricCoord<E> {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
-    ellipsoid: PhantomData<E>,
+impl<E> PartialEq for GeodeticCoord<E> {
+    #[inline]
+    fn eq(&self, other: &GeodeticCoord<E>) -> bool {
+        let &GeodeticCoord { lat: slat, lon: slon, hgt: shgt, .. } = self;
+        let &GeodeticCoord { lat: olat, lon: olon, hgt: ohgt, .. } = other;
+        (slat, slon, shgt) == (olat, olon, ohgt)
+    }
 }
 
-impl<E: Ellipsoid> Debug for GeocentricCoord<E> {
+impl<E> ApproxEq for GeodeticCoord<E> {
+    type Epsilon = f64;
+
+    fn default_epsilon() -> f64 {
+        f64::default_epsilon()
+    }
+
+    fn default_max_relative() -> f64 {
+        f64::default_epsilon()
+    }
+
+    fn default_max_ulps() -> u32 {
+        f64::default_max_ulps()
+    }
+
+    fn relative_eq(&self, other: &GeodeticCoord<E>, epsilon: f64, max_relative: f64) -> bool {
+        self.lat.get().relative_eq(&other.lat.get(), epsilon, max_relative) &&
+            self.lon.get().relative_eq(&other.lon.get(), epsilon, max_relative) &&
+            self.hgt.relative_eq(&other.hgt, epsilon, max_relative)
+    }
+
+    fn ulps_eq(&self, other: &GeodeticCoord<E>, epsilon: f64, max_ulps: u32) -> bool {
+        self.lat.get().ulps_eq(&other.lat.get(), epsilon, max_ulps) &&
+            self.lon.get().ulps_eq(&other.lon.get(), epsilon, max_ulps) &&
+            self.hgt.ulps_eq(&other.hgt, epsilon, max_ulps)
+    }
+}
+
+impl<E: Ellipsoid> Debug for GeodeticCoord<E> {
     fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
-        f.debug_struct(&format!("GeocentricCoord<{}>", E::name()))
-            .field("x", &self.x)
-            .field("y", &self.y)
-            .field("z", &self.z)
+        f.debug_struct(&format!("GeodeticCoord<{}>", E::name()))
+            .field("lat", &self.lat)
+            .field("lon", &self.lon)
+            .field("hgt", &self.hgt)
             .finish()
     }
 }
 
-// This can't be derived because `E` can be an non-`PartialEq` type.
-impl<E> PartialEq for GeocentricCoord<E> {
+impl<E> Clone for GeodeticCoord<E> {
     #[inline]
-    fn eq(&self, other: &GeocentricCoord<E>) -> bool {
-        let &GeocentricCoord { x: sx, y: sy, z: sz, .. } = self;
-        let &GeocentricCoord { x: ox, y: oy, z: oz, .. } = other;
-        (sx, sy, sz) == (ox, oy, oz)
+    fn clone(&self) -> Self {
+        GeodeticCoord {
+            lat: self.lat,
+            lon: self.lon,
+            hgt: self.hgt,
+            _ellipsoid: PhantomData,
+        }
     }
 }
 
-impl<E> GeocentricCoord<E> {
+impl<E> Copy for GeodeticCoord<E> {
+}
+
+/// A position on the earth represented by x [m], y [m], and z [m] in geocentric (ECEF) coordinate.
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct GeocentricCoord {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl GeocentricCoord {
     #[inline]
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         GeocentricCoord {
             x: x,
             y: y,
             z: z,
-            ellipsoid: PhantomData,
         }
     }
 }
 
-impl<E: Ellipsoid> From<GeocentricCoord<E>> for GeodeticCoord {
-    fn from(xyz: GeocentricCoord<E>) -> GeodeticCoord {
+impl ApproxEq for GeocentricCoord {
+    type Epsilon = f64;
+
+    fn default_epsilon() -> f64 {
+        f64::default_epsilon()
+    }
+
+    fn default_max_relative() -> f64 {
+        f64::default_epsilon()
+    }
+
+    fn default_max_ulps() -> u32 {
+        f64::default_max_ulps()
+    }
+
+    fn relative_eq(&self, other: &GeocentricCoord, epsilon: f64, max_relative: f64) -> bool {
+        self.x.relative_eq(&other.x, epsilon, max_relative) &&
+            self.y.relative_eq(&other.y, epsilon, max_relative) &&
+            self.z.relative_eq(&other.z, epsilon, max_relative)
+    }
+
+    fn ulps_eq(&self, other: &GeocentricCoord, epsilon: f64, max_ulps: u32) -> bool {
+        self.x.ulps_eq(&other.x, epsilon, max_ulps) &&
+            self.y.ulps_eq(&other.y, epsilon, max_ulps) &&
+            self.z.ulps_eq(&other.z, epsilon, max_ulps)
+    }
+}
+
+impl<E: Ellipsoid> From<GeocentricCoord> for GeodeticCoord<E> {
+    fn from(xyz: GeocentricCoord) -> GeodeticCoord<E> {
         if xyz ==
             (GeocentricCoord {
                 x: 0.0,
                 y: 0.0,
                 z: 0.0,
-                ellipsoid: PhantomData,
             })
         {
             GeodeticCoord::new(Radians::new(0.0), Radians::new(0.0), 0.0)
         } else {
-            let GeocentricCoord { x, y, z, .. } = xyz;
+            let GeocentricCoord { x, y, z } = xyz;
 
             let radius = E::radius();
             let polar_radius = E::polar_radius();
@@ -141,8 +212,8 @@ impl<E: Ellipsoid> From<GeocentricCoord<E>> for GeodeticCoord {
     }
 }
 
-impl<E: Ellipsoid> From<GeodeticCoord> for GeocentricCoord<E> {
-    fn from(blh: GeodeticCoord) -> GeocentricCoord<E> {
+impl<E: Ellipsoid> From<GeodeticCoord<E>> for GeocentricCoord {
+    fn from(blh: GeodeticCoord<E>) -> GeocentricCoord {
         let (lat, lon) = (blh.lat.get(), blh.lon.get());
 
         // Prime vertical's radius of carvative
@@ -152,18 +223,18 @@ impl<E: Ellipsoid> From<GeodeticCoord> for GeocentricCoord<E> {
             x: (pv_rc + blh.hgt) * f64::cos(lat) * f64::cos(lon),
             y: (pv_rc + blh.hgt) * f64::cos(lat) * f64::sin(lon),
             z: (pv_rc * (1.0 - E::ecc().powi(2)) + blh.hgt) * f64::sin(lat),
-            ellipsoid: PhantomData,
         }
     }
 }
 
 #[test]
 fn test_pos_conversion() {
-    use ellipsoid::{WGS84, GRS80};
+    use ellipsoid::WGS84;
 
-    let blh = GeodeticCoord::from_degrees(Degrees::new(31.0), Degrees::new(131.0), 100.0);
-    assert_eq!(GeodeticCoord::from(GeocentricCoord::<WGS84>::from(blh)), blh);
+    let blh = GeodeticCoord::<WGS84>::from_degrees(Degrees::new(31.0), Degrees::new(131.0), 100.0);
+    assert_relative_eq!(GeodeticCoord::from(GeocentricCoord::from(blh)), blh);
 
-    let xyz = GeodeticCoord::from_degrees(Degrees::new(33.43), Degrees::new(135.7), 100.0);
-    assert_eq!(GeodeticCoord::from(GeocentricCoord::<GRS80>::from(xyz)), xyz);
+    let xyz = GeocentricCoord::new(-3957314.620, 3310254.137, 3737540.043);
+    assert_relative_eq!(GeocentricCoord::from(GeodeticCoord::<WGS84>::from(xyz)), xyz,
+        epsilon = 0.1);
 }
